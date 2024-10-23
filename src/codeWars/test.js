bd51a4fbe6cdf106c731e5940495f726dd1262ea
Alexander <makedonski_rulez@mail.ru>
2024-10-23 17:55:20 +0300

    Каждое слово с заглавной буквы
    Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
    Jaden-Cased: "How Can Mirrors Be Real If Our Eyes Aren't Real"

--- src/codeWars/test.js
+++ src/codeWars/test.js
@@ -1,14 +1,9 @@
-function peopleWithAgeDrink(old) {
-
-    return old < 14 ? 'drink toddy' : old < 18 ? 'drink coke' : old < 21 ? 'drink beer' : 'drink whisky'
-
-/*    if (old < 14) {
-        return 'drink toddy'
-    } else if (old < 18) {
-        return 'drink coke'
-    } else if (old < 21) {
-        return 'drink beer'
-    } else {
-        return 'drink whisky'
-    }*/
+function Word() {
+   let str = "How can mirrors be real if our eyes aren't real";
+    console.log(str)
+   let result = str.split(' ').map(el => el[0].toUpperCase() + el.slice(1)).join(' ')
+    console.log(result)
+    return result
 }
+
+Word()
