f1c434cf08e039e426ff54291e0caa78b0b3f383
Alexander <makedonski_rulez@mail.ru>
2024-10-23 18:20:14 +0300

    Сортировка положительных чисел по убыванию

--- src/codeWars/test.js
+++ src/codeWars/test.js
@@ -1,9 +1,10 @@
-function Word() {
-   let str = "How can mirrors be real if our eyes aren't real";
-    console.log(str)
-   let result = str.split(' ').map(el => el[0].toUpperCase() + el.slice(1)).join(' ')
-    console.log(result)
-    return result
+function descendingOrder(n){
+    if (n > 0) {
+        return n.toString().split('').sort((a,b) => b - a).join('')
+/*        return parseInt(String(n).split('').sort().reverse().join(''))*/
+    }
+    //...
 }
 
-Word()
+descendingOrder(58716)
+console.log(descendingOrder(58716))
